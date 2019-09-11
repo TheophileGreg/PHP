2ceb8cbf691789d969c034a668772203a24dc089
gregoiret <gregoiret@K131-15.dep-info.iutmontp.univ-montp2.fr>
2019-09-11 10:43:25 +0200

    ajout des premiers exos du TD2

---
+++ Model.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Conf.php';
+
+
+	class Model{
+		public static $pdo;
+		public static function Init(){
+
+			$hostname;
+			$database_name;
+			$login;
+			$password;
+
+
+		}
+	}
+
+	Model::Init();
---
+++ testModel.php
@@ -0,0 +1,7 @@
+
+
+<?php
+require_once "Model.php";
+echo "Connexion réussie !" ;
+?>
+
